bde4b5c0bfde7e653970bf9c6b3d1033b40de29d
sfeeling <sfeeling@outlook.com>
2018-05-19 00:28:33 +0800

    对输入相关的控件做了重构

---
+++ input_grid.py
@@ -0,0 +1,51 @@
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLineEdit, QLabel
+from PyQt5.QtCore import Qt
+
+class InputWid:
+
+    def __init__(self, parent=None, header='', warning=''):
+        self._x = 0
+        self._y = 0
+
+        self.header = QLabel(header, parent)
+        self.header.move(self._x, self._y)
+        self.header.setFixedWidth(45)
+        self.header.setAlignment(Qt.AlignRight | Qt.AlignCenter)
+
+        self.content = QLineEdit(parent)
+        self.content.move(self._x + 50, self._y)
+        self.content.setFixedWidth(130)
+
+        self.warning = QLabel(warning, parent)
+        self.warning.move(self._x + 50, self._y + 30)
+        self.warning.setFixedWidth(140)
+        self.warning.hide()
+
+    def set_header(self, header):
+        self.header.setText(header)
+
+    def set_text(self, text):
+        self.content.setText(text)
+
+    def text(self):
+        return self.content.text()
+
+    def set_warning(self, warning):
+        self.warning.setText(warning)
+
+    def show_warning(self):
+        self.warning.show()
+
+    def hide_warning(self):
+        self.warning.hide()
+
+    def connect(self, func):
+        self.content.textChanged.connect(func)
+
+    def set_location(self, x, y):
+        self._x = x
+        self._y = y
+
+        self.header.move(self._x, self._y)
+        self.content.move(self._x + 50, self._y)
+        self.warning.move(self._x + 50, self._y + 30)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel
-from plot_canvas import PlotCanvas
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QGridLayout
 import matplotlib.pyplot as plt
 import time
+
+from plot_canvas import PlotCanvas
+from input_grid import InputWid
 
 topk_canvas = True
 if 1:
@@ -15,8 +17,8 @@ if 1:
             self.title = '星巴克门店分布'
             self.left = 100
             self.top = 100
-            self.width = 1500
-            self.height = 900
+            self.width = 1000
+            self.height = 600
 
             self.initUI()
 
@@ -24,76 +26,54 @@ if 1:
             self.setWindowTitle(self.title)
             self.setGeometry(self.left, self.top, self.width, self.height)
 
-            #self.m = PlotCanvas(self, width=8, height=6)
-            self.m = PlotCanvas(self, width=12, height=9)
-            self.m.move(300, 0)
+            self.m = PlotCanvas(self, width=8, height=6)
+            self.m.move(200, 0)
 
             if topk_canvas:
-                self.lbl_lon = QLabel('Lon:', self)
-                self.lbl_lon.move(50, 150)
+                self.input_lon = InputWid(self, 'Lon:', '输入区间为[-180,180]')
+                self.input_lon.set_location(0, 100)
+                self.input_lon.connect(self.text_changed)
 
-                self.line_lon = QLineEdit(self)
-                self.line_lon.setFixedWidth(160)
-                self.line_lon.move(100, 150)
-                self.line_lon.textChanged.connect(self.text_changed)
+                self.input_lat = InputWid(self, 'Lat', '输入区间为[-90,90]')
+                self.input_lat.set_location(0, 170)
+                self.input_lat.connect(self.text_changed)
 
-                self.warn_lon = QLabel('输入区间为[-180,180]', self)
-                self.warn_lon.setFixedWidth(200)
-                self.warn_lon.move(100, 180)
-                self.warn_lon.hide()
+                self.input_k = InputWid(self, 'K:', '输入区间为[1,25600]')
+                self.input_k.set_location(0, 240)
+                self.input_k.connect(self.text_changed)
 
-                self.lbl_lat = QLabel('Lat:', self)
-                self.lbl_lat.move(50, 250)
-
-                self.line_lat = QLineEdit(self)
-                self.line_lat.setFixedWidth(160)
-                self.line_lat.move(100, 250)
-                self.line_lat.textChanged.connect(self.text_changed)
-
-                self.warn_lat = QLabel('输入区间为[-90,90]', self)
-                self.warn_lat.setFixedWidth(200)
-                self.warn_lat.move(100, 280)
-                self.warn_lat.hide()
-
-                self.lbl_k = QLabel('  K:', self)
-                self.lbl_k.move(50, 350)
-
-                self.line_k = QLineEdit(self)
-                self.line_k.setFixedWidth(160)
-                self.line_k.move(100, 350)
-                self.line_k.textChanged.connect(self.text_changed)
-
-                self.warn_k = QLabel('输入区间为[1,25600]', self)
-                self.warn_k.setFixedWidth(200)
-                self.warn_k.move(100, 380)
-                self.warn_k.hide()
+                self.input_range = InputWid(self, 'Range:')
+                self.input_range.set_location(0, 310)
 
                 self.button_check = QPushButton('查询', self)
-                self.button_check.move(40, 450)
+                self.button_check.move(20, 400)
+                self.button_check.setFixedWidth(70)
                 self.button_check.clicked.connect(self.button_check_on_click)
 
                 self.button_k = QPushButton('展示', self)
-                self.button_k.move(160, 450)
+                self.button_k.move(110, 400)
+                self.button_k.setFixedWidth(70)
                 self.button_k.clicked.connect(self.button_k_on_click)
 
                 self.lbl_delay = QLabel('此次查询时延为',self)
-                self.lbl_delay.setFixedWidth(220)
-                self.lbl_delay.move(40, 500)
+                self.lbl_delay.setFixedWidth(140)
+                self.lbl_delay.move(20, 440)
                 self.lbl_delay.hide()
+
             self.show()
 
         @pyqtSlot()
         def text_changed(self):
-            self.warn_lon.hide()
-            self.warn_lat.hide()
-            self.warn_k.hide()
+            self.input_lon.hide_warning()
+            self.input_lat.hide_warning()
+            self.input_k.hide_warning()
             self.lbl_delay.hide()
 
         @pyqtSlot()
         def button_check_on_click(self):
-            lon = float(self.line_lon.text())
-            lat = float(self.line_lat.text())
-            k = int(self.line_k.text())
+            lon = float(self.input_lon.text())
+            lat = float(self.input_lat.text())
+            k = int(self.input_k.text())
             if lon >= -180 and lon <= 180 and lat >= -90 and lat <= 90 and k >= 1 and k <= 25600:
                 start = time.clock()
                 if not self.m.get_point() == None:
@@ -107,11 +87,11 @@ if 1:
                 self.lbl_delay.show()
             else:
                 if lon < -180 or lon > 180:
-                    self.warn_lon.show()
+                    self.input_lon.show_warning()
                 if lat < -90 or lat > 90:
-                    self.warn_lat.show()
+                    self.input_lat.show_warning()
                 if k < 1 or k > 25600:
-                    self.warn_k.show()
+                    self.input_k.show_warning()
 
 
 
@@ -121,8 +101,8 @@ if 1:
             x = []
             y = []
 
-            lon = float(self.line_lon.text())
-            lat = float(self.line_lat.text())
+            lon = float(self.input_lon.text())
+            lat = float(self.input_lat.text())
 
             i = 1250
             while i <= 25600:
@@ -139,11 +119,12 @@ if 1:
                     y.append(delay)
                 else:
                     if lon < -180 or lon > 180:
-                        self.warn_lon.show()
+                        self.input_lon.show_warning()
                     if lat < -90 or lat > 90:
-                        self.warn_lat.show()
+                        self.input_lat.show_warning()
                     if k < 1 or k > 25600:
-                        self.warn_k.show()
+                        self.input_k.show_warning()
+
                 i += 1250
 
             plt.scatter(x, y, color='k', s=25, marker="o")
